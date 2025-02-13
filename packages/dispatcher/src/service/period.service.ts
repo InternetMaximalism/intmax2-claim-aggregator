17891cb08b620dcc440a18c18e2a13aba0059e57
signature18632 <intmax18@gmail.com>
2025-02-13 09:30:32 +0700

    period claim migration

---
+++ packages/dispatcher/src/service/period.service.ts
@@ -0,0 +1,13 @@
+import { ClaimAbi, createNetworkClient, logger } from "@intmax2-claim-aggregator/shared";
+import { getContract } from "viem";
+import { CLAIM_CONTRACT_ADDRESS } from "../constants";
+
+export const getCurrentPeriod = async (ethereumClient: ReturnType<typeof createNetworkClient>) => {
+  const contract = getContract({
+    address: CLAIM_CONTRACT_ADDRESS,
+    abi: ClaimAbi,
+    client: ethereumClient,
+  });
+  const currentPeriod = await contract.read.getCurrentPeriod();
+  logger.info(`Current period: ${currentPeriod}`);
+};
