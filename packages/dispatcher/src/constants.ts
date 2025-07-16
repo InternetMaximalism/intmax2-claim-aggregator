23c57018808a5add5c9f8cf2e13a0969d37ddb8f
signature18632 <intmax18@gmail.com>
2025-07-16 20:48:51 +0700

    period env

--- packages/dispatcher/src/constants.ts
+++ packages/dispatcher/src/constants.ts
@@ -1,6 +1,8 @@
+import { config } from "@intmax2-claim-aggregator/shared";
+
 // period
-export const PERIOD_BATCH_SIZE = 2;
-export const PERIOD_BATCH_DELAY = 1000;
+export const PERIOD_BATCH_SIZE = config.PERIOD_BATCH_SIZE;
+export const PERIOD_BATCH_DELAY = config.PERIOD_BATCH_DELAY;
 
 // event
 export const MAX_RECIPIENT_BATCH_SIZE = 100;
